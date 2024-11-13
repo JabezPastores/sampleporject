e20bd0c5db15cb9ed4a597dfef32e2592d06070f
App Dev <appdev@Apps-Mac-mini.sfm.edu.ph>
2024-11-13 08:16:09 +0800

    Change the naming of the components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import * as React from 'react';
 
-function App() {
-  const list = [
-    {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-    },
-    {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-    },
-    ];
-    return (
-      <div>
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+},
+{
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+},
+];
+
+const App = () => {
+  return (
+    <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <ul>
+      <List />
+    </div>
+); }
+const Search =  () => {
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type="text" />
+    </div>
+  );
+}
+const List = () => {
+  return (
+    <ul>
       {list.map(function (item) {
         return (
           <li key={item.objectID}>
@@ -34,22 +48,7 @@ function App() {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
-
-          </li>
-        );
-      })}
-      </ul>
-      
-    </div>
-  );
-}
-function Search (){
-  return (
-    <div>
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type="text" />
-    </div>
-  );
-}
-
+</li> );
+})} </ul>
+); }
 export default App;
